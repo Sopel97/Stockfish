4fac7024131e0d3605c01b9ff338c8ce7d3f3bf0
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-11-11 01:31:40 +0100

    Make pawn traits features cover both sides for each input slice, handle perspective.
    Fix kAnyPawnMoved update.
    
    asd

--- src/nnue/features/pawn_traits.cpp
+++ src/nnue/features/pawn_traits.cpp
@@ -3,25 +3,28 @@
 
 namespace Eval::NNUE::Features::Detail {
 
-    void append_active_indices(
-        Bitboard& bb,
-        IndexType offset,
-        IndexList* active)
-    {
-        while(bb)
-        {
-            Square sq = pop_lsb(&bb);
-            active->push_back(offset + sq);
-        }
+    // Orient a square according to perspective (flip rank for black)
+    static inline Square orient(Color perspective, Square s) {
+      return Square(int(s) ^ (bool(perspective) * SQ_A8));
     }
 
-    void append_score_indices(
-        Value score,
+    static inline IndexType make_index(Color perspective, Square s, Color color) {
+        return IndexType(orient(perspective, s) + bool(perspective ^ color) * SQUARE_NB);
+    }
+
+    void append_active_indices(
+        Bitboard bbs[2],
+        Color perspective,
         IndexType offset,
         IndexList* active)
     {
-        IndexType bucket = score == 0 ? 0 : 1 + msb(abs(score));
-        active->push_back(offset + bucket);
+        for (auto color : Colors) {
+            Bitboard bb = bbs[color];
+            while (bb) {
+              Square s = pop_lsb(&bb);
+              active->push_back(offset + make_index(perspective, s, color));
+            }
+        }
     }
 
 }  // namespace Eval::NNUE::Features::Detail
