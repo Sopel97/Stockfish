af1dbe03a42c73bf5817b6eacffb65dc5acbcbdd
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-12-15 19:07:37 +0100

    change arch

--- src/nnue/architectures/halfkae5_256x2-32-32.h
+++ src/nnue/architectures/halfkae5_256x2-32-32.h
@@ -27,6 +27,8 @@
 #include "nnue/layers/input_slice.h"
 #include "nnue/layers/affine_transform.h"
 #include "nnue/layers/clipped_relu.h"
+#include "nnue/layers/relu.h"
+#include "nnue/layers/inner_product.h"
 
 namespace Eval::NNUE {
 
@@ -42,8 +44,8 @@ namespace Eval::NNUE {
         // Define network structure
         using InputLayer = InputSlice<kTransformedFeatureDimensions * 2>;
         using HiddenLayer1 = ClippedReLU<AffineTransform<InputLayer, 32>>;
-        using HiddenLayer2 = ClippedReLU<AffineTransform<HiddenLayer1, 32>>;
-        using OutputLayer = AffineTransform<HiddenLayer2, 1>;
+        using HiddenLayer2 = ReLU<AffineTransform<HiddenLayer1, 32>>;
+        using OutputLayer = InnerProduct<HiddenLayer2>;
 
     }  // namespace Layers
 
