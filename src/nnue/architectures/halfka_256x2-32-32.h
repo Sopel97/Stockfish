7832c5f5f449303090c3851e9bde8b31a8bb7380
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-06-10 12:06:29 +0200

    change arch

--- src/nnue/architectures/halfka_256x2-32-32.h
+++ src/nnue/architectures/halfka_256x2-32-32.h
@@ -22,7 +22,7 @@
 #define NNUE_HALFKA_256X2_32_32_H_INCLUDED
 
 #include "../features/feature_set.h"
-#include "../features/half_ka.h"
+#include "../features/half_ka_e3.h"
 
 #include "../layers/input_slice.h"
 #include "../layers/affine_transform.h"
@@ -32,7 +32,7 @@ namespace Stockfish::Eval::NNUE {
 
 // Input features used in evaluation function
 using RawFeatures = Features::FeatureSet<
-    Features::HalfKA<Features::Side::kFriend>>;
+    Features::HalfKA_E3<Features::Side::kFriend>>;
 
 // Number of input feature dimensions after conversion
 constexpr IndexType kTransformedFeatureDimensions = 512;
