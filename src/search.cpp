7873619e12b0230673c56198a165601cb2244e75
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-08-23 01:27:09 +0200

    some extension on rank 2/7 capture

--- src/search.cpp
+++ src/search.cpp
@@ -1098,6 +1098,14 @@ moves_loop: // When in check, search starts here
                && abs(ss->staticEval) > Value(100))
           extension = 1;
 
+      if (   !rootNode
+          && depth >= 7
+          && extension == 0
+          && captureOrPromotion
+          && pos.count<ALL_PIECES>() > 20
+          && (to_sq(move) & (Rank2BB | Rank7BB)))
+          extension = 1;
+
       // Add extension to new depth
       newDepth += extension;
       ss->doubleExtensions = (ss-1)->doubleExtensions + (extension == 2);
