18092da99b2c076776c53f3b88298dc817b3c0e6
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-12-23 17:16:08 +0100

    nn-exp264-run3-epoch399.nnue

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-39b15f4468ca.nnue"
+  #define EvalFileDefaultName   "nn-6b70b78c386e.nnue"
 
   namespace NNUE {
 
