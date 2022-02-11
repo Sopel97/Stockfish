22c4e369c9c7b7476eb2263801f40f2bd90f6698
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2022-02-11 15:39:25 +0100

    always lazy

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -159,7 +159,7 @@ namespace Stockfish::Eval::NNUE {
 
     const std::size_t bucket = (pos.count<ALL_PIECES>() - 1) / 4;
     const auto psqt = featureTransformer->transform(pos, transformedFeatures, bucket);
-    const bool lazy = abs(psqt) > 800;
+    const bool lazy = true;
     const auto positional = network[bucket]->propagate(transformedFeatures, lazy);
 
     // Give more value to positional evaluation when adjusted flag is set
