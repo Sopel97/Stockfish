945d3495793afe2ce8fdaeaa98e8edc15ac684bb
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2024-05-24 13:29:35 +0200

    dsa

--- src/numa.h
+++ src/numa.h
@@ -119,7 +119,7 @@ public:
     std::cout << "created numa config\n";
   }
 
-  static std::set<CpuIndex> get_process_affinity() const {
+  static std::set<CpuIndex> get_process_affinity() {
     std::set<CpuIndex> cpus;
 
 #if defined(__linux__)
@@ -219,7 +219,7 @@ public:
       const CpuIndex numCpus = CpuIndex{std::max<CpuIndex>(1, std::thread::hardware_concurrency())};
       for (CpuIndex c = 0; c < numCpus; ++c)
         if (is_cpu_allowed(c))
-          cfg.add_cpu_to_node(NodeIndex{0}, c)
+          cfg.add_cpu_to_node(NumaIndex{0}, c)
     }
 
 #elif defined(_WIN32)
